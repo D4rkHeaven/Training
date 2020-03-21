c0f9eae947668065f03692ffb584d0af4d1c52e1
DarkHeaven <l.dmi3.v@gmail.com>
2020-03-21 17:40:03 +0400

    Add constants

--- src/main/java/lopatin/task3/CustomClassLoader.java
+++ src/main/java/lopatin/task3/CustomClassLoader.java
@@ -9,6 +9,9 @@ import java.io.*;
  */
 @Slf4j
 public class CustomClassLoader extends ClassLoader {
+    private static final String CLASSPATH = "C:/myClasses/";
+    private static final String FILE_EXTENSION = ".class";
+
     /**
      * Get bytecode from file and load class in runtime
      *
@@ -19,11 +22,11 @@ public class CustomClassLoader extends ClassLoader {
     @Override
     public Class<?> findClass(String className) throws ClassNotFoundException {
         try {
-            byte[] bytes = fetchClassFromFS("C:/myClasses/" + className + ".class");
-            log.info("File .class loaded!");
+            byte[] bytes = fetchClassFromFS(CLASSPATH + className + FILE_EXTENSION);
+            log.info("File {}{}{} loaded!", CLASSPATH, className, FILE_EXTENSION);
             return defineClass(className, bytes, 0, bytes.length);
         } catch (FileNotFoundException e) {
-            log.error("File .class not found! " + e.getMessage());
+            log.error("File not found! " + e.getMessage());
             return super.findClass(className);
         } catch (IOException e) {
             log.error("IO error while loading class! " + e.getMessage());
@@ -35,7 +38,7 @@ public class CustomClassLoader extends ClassLoader {
         InputStream inputStream = new FileInputStream(new File(path));
         long length = new File(path).length();
         if (length > Integer.MAX_VALUE) {
-            log.error("File {} too large!" + path);
+            log.error("File {} too large!", path);
         }
         byte[] bytes = new byte[(int) length];
         int offset = 0;
